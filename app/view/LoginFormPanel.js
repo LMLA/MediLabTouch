ac4229aa62b25c8ddf3531278c42fce42c1a6e3f
Nicolas Echavarria <nicolasechavarria@negraphics.com>
2012-06-20 16:47:36 -0500

    Updated app title

--- app/view/LoginFormPanel.js
+++ app/view/LoginFormPanel.js
@@ -22,7 +22,7 @@ Ext.define('MediLab.view.LoginFormPanel', {
             {
                 xtype: 'titlebar',
                 docked: 'top',
-                title: 'MediLab Touch Jaime '
+                title: 'MediLab Touch '
             },
             {
                 xtype: 'fieldset',
