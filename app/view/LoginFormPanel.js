e979240a800264c0a39ab1a73bbc7ad283042636
Nicolas Echavarria <nicolasechavarria@negraphics.com>
2012-06-21 12:13:38 -0500

    Revert de cambios con Architect sobre proyecto en IntelliJ IDEA. Test.

--- app/view/LoginFormPanel.js
+++ app/view/LoginFormPanel.js
@@ -22,7 +22,7 @@ Ext.define('MediLab.view.LoginFormPanel', {
             {
                 xtype: 'titlebar',
                 docked: 'top',
-                title: 'MediLab Touch IntelliJ'
+                title: 'MediLab Touch'
             },
             {
                 xtype: 'fieldset',
